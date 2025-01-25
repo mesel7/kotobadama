3cbb1190f0c5aec49982b596da310d77348f6644
mesel7 <cejhans1520@gmail.com>
2025-01-26 04:55:32 +0900

    VocaContext reducer, dispatch 구현, mockData 구성

--- src/contexts/VocaContext.jsx
+++ src/contexts/VocaContext.jsx
@@ -1,25 +1,100 @@
-import React, { useReducer } from "react"
+import React, { useEffect, useReducer } from "react"
 
 export const VocaStateContext = React.createContext();
 export const VocaDispatchContext = React.createContext();
 
+/*
+const mockData = [
+    {
+        id: 0,
+        name: "sample",
+        wordCount: 2,
+        createdAt: "25.01.26", // Date 객체가 아닌 단순 문자열
+        description: "JLPT",
+        words: [
+            {
+                wordKanji: "行く",
+                wordKana: "いく",
+                meaning: "가다",
+                status: "known(암기 완료)/partial(부분 암기)/unknown(암기 필요)"
+            },
+            {
+                wordKanji: "心",
+                wordKana: "こころ",
+                meaning: "마음",
+                status: "known"
+            }
+        ]
+    },
+    {}
+];
+*/
+
 const reducer = (state, action) => {
     switch(action.type) {
-        case "INIT": return action.data;
-        case "CREATE": return action.data;
-        case "UPDATE": return action.data;
-        case "DELETE": return action.data;
-        default: return state;
+        case "INIT": {
+            const vocaList = localStorage.getItem("vocaList");
+            if (vocaList) {
+                const newState = JSON.parse(vocaList);
+                return newState;
+            } else {
+                return action.data;
+            }
+        }
+
+        case "CREATE": {
+            const newState = [...state, action.data];
+            localStorage.setItem("vocaList", JSON.stringify(newState));
+            return newState;
+        }
+
+        case "UPDATE": {
+            const newState = state.map((it) => 
+                String(it.id) === String(action.data.id) ? action.data : it
+            );
+            localStorage.setItem("vocaList", JSON.stringify(newState));
+            return newState;
+        }
+
+        case "DELETE": {
+            const newState = state.filter((it) => 
+                String(it.id) !== String(action.data.id)
+            );
+            localStorage.setItem("vocaList", JSON.stringify(newState));
+            return newState;
+        }
+        default: {
+            return state;
+        }
     }
 };
 
 export const VocaProvider = ({ children }) => {
     const [state, dispatch] = useReducer(reducer, []);
 
+    useEffect(() => {
+        dispatch({ type: "INIT", data: [] });
+    }, []);
+
+    // 단어장 1개 생성
+    const onCreate = (newVoca) => {
+        dispatch({ type: "CREATE", data: newVoca });
+    };
+
+    // 단어장 1개 업데이트(수정)
+    const onUpdate = (targetVoca) => {
+        dispatch({ type: "UPDATE", data: targetVoca });
+    };
+
+    // 단어장 1개 삭제
+    const onDelete = (targetVoca) => {
+        dispatch({ type: "DELETE", data: targetVoca });
+    };
+
     return (
         <>
             <VocaStateContext.Provider value={state}>
-                <VocaDispatchContext.Provider>
+                <VocaDispatchContext.Provider value={{ onCreate, onUpdate, onDelete }}>
                     {children}
                 </VocaDispatchContext.Provider>
             </VocaStateContext.Provider>

08103106ba91c331e511780991c2aa4724665c66
mesel7 <cejhans1520@gmail.com>
2025-01-26 15:26:59 +0900

    Button 컴포넌트 추가

---
+++ src/components/common/Button.jsx
@@ -0,0 +1,9 @@
+import "./Button.css";
+
+const Button = ({ text, type, onClick }) => {
+    return (
+        <button className={["button", `button_${type}`].join(" ")} onClick={onClick}>{text}</button>
+    );
+};
+
+export default Button;
--- src/components/list/ListContent.jsx
+++ src/components/list/ListContent.jsx
@@ -1,9 +1,11 @@
 import ListItem from "./ListItem";
 import "./ListContent.css";
+import Button from "../common/Button";
 
 const ListContent = () => {
     return (
         <div className="list-content">
+            <Button text={"+ 단어장 추가"} />
             <ListItem />
             <ListItem />
             <ListItem />

071ec279731ed01bcd5f30cce806daf8132d566c
mesel7 <cejhans1520@gmail.com>
2025-02-03 05:18:45 +0900

    단어 상태 progress-bar 추가

--- src/components/list/ListItem.jsx
+++ src/components/list/ListItem.jsx
@@ -4,7 +4,7 @@ import { formatDate, icons } from "../../utils";
 import "./ListItem.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-const ListItem = ({ id, name, wordCount, createdAt, description }) => {
+const ListItem = ({ id, name, wordCount, createdAt, description, words }) => {
     const navigate = useNavigate();
 
     const handleVocaListClick = () => {
@@ -15,6 +15,23 @@ const ListItem = ({ id, name, wordCount, createdAt, description }) => {
         navigate(`/edit/${id}`);
     };
 
+    const getWordStatus = () => {
+        const { known, partial, unknown } = words.reduce((acc, word) => {
+            if (word.status === "known") {
+                acc.known += 1;
+            } else if (word.status === "partial") {
+                acc.partial += 1;
+            } else {
+                acc.unknown += 1;
+            }
+            return acc;
+        }, { known: 0, partial: 0, unknown: 0 });
+
+        return [known, partial, unknown];
+    };
+
+    const [known, partial, unknown] = getWordStatus();
+
     return (
         <div className="list-item">
             <div className="img-wrapper" onClick={handleVocaListClick}>
@@ -22,7 +39,16 @@ const ListItem = ({ id, name, wordCount, createdAt, description }) => {
             </div>
             <div className="content-wrapper" onClick={handleVocaListClick}>
                 <div className="name">{name}</div>
-                <div className="word">{`${wordCount} 단어`}</div>
+                <div className="word-status">
+                    {`${known} | ${partial} | ${unknown} 🔹 ${wordCount}`}
+                </div>
+                <div className="progress-bar-container">
+                    <div className="progress-bar">
+                        <div className="progress-known" style={{ width: `${(known / wordCount) * 100}%` }} />
+                        <div className="progress-partial" style={{ width: `${(partial / wordCount) * 100}%` }} />
+                        <div className="progress-unknown" style={{ width: `${(unknown / wordCount) * 100}%` }} />
+                    </div>
+                </div>
                 <div className="created-at">{formatDate(createdAt)}</div>
                 <div className="description">{description}</div>
             </div>
